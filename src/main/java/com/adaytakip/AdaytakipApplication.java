6434fa7b407235412aef3d978e7815c1dae591a1
Caner Özkan <cenarozkan@gmail.com>
2021-03-29 17:46:08 +0300

    projeyi oluşturma
    projeyi oluşturma

---
+++ src/main/java/com/adaytakip/AdaytakipApplication.java
@@ -0,0 +1,13 @@
+package com.adaytakip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaytakipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaytakipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adaytakip/AdaytakipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaytakip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaytakipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
